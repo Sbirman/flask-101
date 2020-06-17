98168e29293b0ddd3942e628fc2b39a1fe4c4c7f
AIT TALEB Ilyas <ilyas.aittaleb@gmail.com>
2020-06-17 15:18:59 +0200

    correction jsonify

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,18 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World !!"
+
+PRODUCTS = {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Le Wagon'}
+}
+
+@app.route('/api/v1/products')
+def products():
+    return jsonify(PRODUCTS)
+
